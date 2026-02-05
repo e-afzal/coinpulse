aba9315508d99645963e679c4508b82e933a2dbc
e-afzal <essam.afzal@outlook.com>
2026-02-06 01:14:48 +0400

    Added try/catch to dynamic components to avoid critical error

--- components/home/CoinsOverview.tsx
+++ components/home/CoinsOverview.tsx
@@ -1,25 +1,41 @@
+import React from "react";
 import Image from "next/image";
+
+// LIB
+import { formatCurrency } from "@/lib/utils";
+
+// LOCAL COMPONENT
+import { CoinsOverviewFallback } from "@/components/home/fallback";
 
 // ACTION
 import { fetcher } from "@/lib/coingecko.actions";
 
-// LOCAL COMPONENT
-import DataTable from "@/components/DataTable";
-
-// UTIL
-import { formatCurrency } from "@/lib/utils";
-
 export default async function CoinsOverview() {
-    const coin = await fetcher<CoinDetailsData>("/coins/bitcoin", { dex_pair_format: "symbol" });
+    let coin;
+
+    // HANDLE ERROR using Try/Catch
+    try {
+        coin = await fetcher<CoinDetailsData>("/coins/bitcoin", {
+            dex_pair_format: "symbol"
+        });
+    } catch (error) {
+        console.error("Error fetching coin overview:", error);
+        return <CoinsOverviewFallback/>;
+    }
+
+    // Show UI with data, if no error
     return (
-        <div className="coin-overview">
+        <div id="coin-overview">
             <div className="header pt-2">
                 <Image src={coin.image.large} alt={coin.name} width={56} height={56}/>
                 <div className="info">
-                    <p>{coin.name} / {coin.symbol.toUpperCase()}</p>
+                    <p>
+                        {coin.name} / {coin.symbol.toUpperCase()}
+                    </p>
                     <h1>{formatCurrency(coin.market_data.current_price.usd)}</h1>
                 </div>
             </div>
         </div>
     );
 }
+
